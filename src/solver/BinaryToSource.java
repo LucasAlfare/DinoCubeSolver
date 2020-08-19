c0813e18b8d5c1bd5d08281d2e4a7ba1323d3919
lucassousa <god-lucas@hotmail.com>
2020-08-18 21:34:40 -0300

    attempting build source from binary data

---
+++ src/solver/BinaryToSource.java
@@ -0,0 +1,21 @@
+package solver;
+
+public class BinaryToSource {
+
+    public static void main(String[] args) {
+        byte[] distances = new byte[19958400];
+        FileUtils.loadFileToArray(distances);
+
+        StringBuilder src = new StringBuilder("package solver;public class Distances{" + "public static final byte[]DISTANCES={");
+
+        int len = distances.length;
+        for (int i = 0; i < len - 1; i++) {
+            src.append(distances[i]).append(",");
+        }
+
+        src.append(distances[len - 1]).append("};");
+        src.append("}");
+
+        FileUtils.writeText(src.toString());
+    }
+}
