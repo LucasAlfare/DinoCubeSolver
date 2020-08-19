25bd1afe560b27899d6ce8c36212656d87197720
lucassousa <god-lucas@hotmail.com>
2020-08-19 09:21:05 -0300

    some state customization and clean up

--- src/solver/Search.java
+++ src/solver/Search.java
@@ -1,49 +1,18 @@
 package solver;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.FileChannel;
-
 import static solver.FileUtils.loadFileToArray;
 import static solver.Generator.Time.timestampOf;
 
 public class Search {
 
     public static byte[] distances = new byte[19958400];
-
-    private static final State m1 = new State(new byte[]{6, 1, 2, 0, 4, 5, 3, 7, 8, 9, 10});
-    private static final State m2 = new State(new byte[]{1, 7, 2, 3, 4, 5, 6, 0, 8, 9, 10});
-    private static final State m3 = new State(new byte[]{0, 2, 4, 3, 1, 5, 6, 7, 8, 9, 10});
-    private static final State m4 = new State(new byte[]{0, 1, 3, 5, 4, 2, 6, 7, 8, 9, 10});
-
-    private static final State m5 = new State(new byte[]{0, 1, 2, 3, 8, 5, 6, 7, 9, 4, 10});
-    private static final State m6 = new State(new byte[]{0, 1, 2, 3, 4, 5, 6, 9, 8, 10, 7});
-
-    private static final State[] moves = new State[]{
-            m1, //1
-            m1.multiply(m1), //-1
-            m2, //2
-            m2.multiply(m2), //-2
-            m3, //3
-            m3.multiply(m3), //-3
-            m4, //4
-            m4.multiply(m4), //-4
-            m5, //5
-            m5.multiply(m5), //-5
-            m6, //6
-            m6.multiply(m6) //-6
-    };
-
     static int[] names = {1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6};
 
     public static void main(String[] args) {
         long a = System.currentTimeMillis();
         loadFileToArray(distances, "distances.dino");
-        //State s = new State(new byte[]{2, 6, 5, 0, 9, 8, 3, 1, 10, 7, 4});
-        //State s = new State(new byte[]{4, 10, 3, 8, 1, 5, 0, 9, 7, 2, 6});
-        State s = new State(new byte[]{1, 9, 2, 5, 4, 7, 6, 3, 10, 8, 0});
+        State s = new State();
+        s.applySequence("1 -2 4 6");
         search(s);
         System.out.println(timestampOf(System.currentTimeMillis() - a));
     }
@@ -58,8 +27,8 @@ public class Search {
                 return;
             }
 
-            for (int i = 0; i < moves.length; i++) {
-                aux = target.multiply(moves[i]);
+            for (int i = 0; i < State.MOVES.length; i++) {
+                aux = target.multiply(State.MOVES[i]);
                 int nextIndex = IndexMapping.evenPermutationToIndex(aux.permutation);
                 if (distances[nextIndex] < distances[currentIndex]) {
                     System.out.print(names[i] + " ");
--- src/solver/State.java
+++ src/solver/State.java
@@ -7,6 +7,28 @@ public class State {
     public byte[] permutation;
     public static final int N_PIECES = 11;
 
+    private static final State M1 = new State(new byte[]{6, 1, 2, 0, 4, 5, 3, 7, 8, 9, 10});
+    private static final State M2 = new State(new byte[]{1, 7, 2, 3, 4, 5, 6, 0, 8, 9, 10});
+    private static final State M3 = new State(new byte[]{0, 2, 4, 3, 1, 5, 6, 7, 8, 9, 10});
+    private static final State M4 = new State(new byte[]{0, 1, 3, 5, 4, 2, 6, 7, 8, 9, 10});
+
+    private static final State M5 = new State(new byte[]{0, 1, 2, 3, 8, 5, 6, 7, 9, 4, 10});
+    private static final State M6 = new State(new byte[]{0, 1, 2, 3, 4, 5, 6, 9, 8, 10, 7});
+
+    //moves order: 1 -1 2 -2 3 -3 4 -4 5 -5 6 -6 (total: 12)
+    public static final State[] MOVES = {
+            M1, M1.multiply(M1),
+            M2, M2.multiply(M2),
+            M3, M3.multiply(M3),
+            M4, M4.multiply(M4),
+            M5, M5.multiply(M5),
+            M6, M6.multiply(M6)
+    };
+
+    public State() {
+        permutation = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+
     public State(byte[] permutation) {
         this.permutation = permutation;
     }
@@ -18,6 +40,46 @@ public class State {
         }
 
         return new State(permutation);
+    }
+
+    public void applySequence(String seq) {
+        String[] names = seq.split(" ");
+        for (String n : names) {
+            applyMoves(Integer.parseInt(n));
+        }
+    }
+
+    public void applyMoves(int... names) {
+        State aux = new State(permutation);
+        for (int n : names) {
+            if (n == 1) {
+                aux = aux.multiply(MOVES[0]);
+            } else if (n == -1) {
+                aux = aux.multiply(MOVES[1]);
+            } else if (n == 2) {
+                aux = aux.multiply(MOVES[2]);
+            } else if (n == -2) {
+                aux = aux.multiply(MOVES[3]);
+            } else if (n == 3) {
+                aux = aux.multiply(MOVES[4]);
+            } else if (n == -3) {
+                aux = aux.multiply(MOVES[5]);
+            } else if (n == 4) {
+                aux = aux.multiply(MOVES[6]);
+            } else if (n == -4) {
+                aux = aux.multiply(MOVES[7]);
+            } else if (n == 5) {
+                aux = aux.multiply(MOVES[8]);
+            } else if (n == -5) {
+                aux = aux.multiply(MOVES[9]);
+            } else if (n == 6) {
+                aux = aux.multiply(MOVES[10]);
+            } else if (n == -6) {
+                aux = aux.multiply(MOVES[11]);
+            }
+        }
+
+        this.copy(aux);
     }
 
     public void copy(State state) {
