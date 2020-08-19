7be9fade2da65ae3105445d3e6dbccba343a28d3
lucassousa <god-lucas@hotmail.com>
2020-08-19 09:02:12 -0300

    rename chanel variable

--- src/solver/FileUtils.java
+++ src/solver/FileUtils.java
@@ -8,18 +8,18 @@ import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
 
 /**
- * Some functions to fast I/O files.
+ * Some functions to fast R/W files.
  */
 public class FileUtils {
 
     public static void writeText(String text, String pathname) {
         try {
             RandomAccessFile rnd = new RandomAccessFile(pathname, "rw");
-            FileChannel fileChannel = rnd.getChannel();
-            ByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, text.length());
+            FileChannel channel = rnd.getChannel();
+            ByteBuffer buf = channel.map(FileChannel.MapMode.READ_WRITE, 0, text.length());
             buf.put(text.getBytes());
             rnd.close();
-            fileChannel.close();
+            channel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,13 +28,13 @@ public class FileUtils {
     public static void writeBinary(byte[] content, String pathname) {
         try {
             RandomAccessFile rnd = new RandomAccessFile(pathname, "rw");
-            FileChannel fileChannel = rnd.getChannel();
-            ByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, content.length);
+            FileChannel channel = rnd.getChannel();
+            ByteBuffer buf = channel.map(FileChannel.MapMode.READ_WRITE, 0, content.length);
             for (byte b : content) {
                 buf.put(b);
             }
             rnd.close();
-            fileChannel.close();
+            channel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,12 +42,12 @@ public class FileUtils {
 
     public static void loadFileToArray(byte[] destination, String pathname) {
         try (FileInputStream stream = new FileInputStream(pathname)) {
-            FileChannel inChannel = stream.getChannel();
-            ByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+            FileChannel channel = stream.getChannel();
+            ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
             buffer.order(ByteOrder.BIG_ENDIAN);
             buffer.get(destination);
             stream.close();
-            inChannel.close();
+            channel.close();
             System.out.println(destination.length + " bytes carregados com sucesso no array especificado.");
         } catch (IOException e) {
             e.printStackTrace();
