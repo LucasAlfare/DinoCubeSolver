1b1e82eca7cb63f9d9a12f49de6c64cd11bb759e
lucassousa <god-lucas@hotmail.com>
2020-08-18 21:38:22 -0300

    clean

--- src/solver/BinaryToSource.java
+++ src/solver/BinaryToSource.java
@@ -6,7 +6,7 @@ public class BinaryToSource {
         byte[] distances = new byte[19958400];
         FileUtils.loadFileToArray(distances, "distances.dino");
 
-        StringBuilder src = new StringBuilder("package solver;public class Distances{" + "public static final byte[]DISTANCES={");
+        StringBuilder src = new StringBuilder("package solver;public class Distances{public static final byte[]DISTANCES={");
 
         int len = distances.length;
         for (int i = 0; i < len - 1; i++) {
